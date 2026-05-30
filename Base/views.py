ae2c4148aaf0002625ae033b29b71098ccb4e13e
Kamini Parmar <kaminiparmar31489@gmail.com>
2026-05-30 18:08:04 +0530

    Remove email temporarily

--- Base/views.py
+++ Base/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render, redirect
-from django.core.mail import send_mail
-from django.conf import settings
 from .models import Contact
 
 
@@ -18,27 +16,6 @@ def home(request):
             email=email,
             number=number,
             content=content
-        )
-
-        subject = f"New Portfolio Contact - {name}"
-
-        message = f"""
-Name: {name}
-
-Email: {email}
-
-Phone: {number}
-
-Message:
-{content}
-"""
-
-        send_mail(
-            subject,
-            message,
-            settings.EMAIL_HOST_USER,
-            ["kaminiparmar31489@gmail.com"],
-            fail_silently=False,
         )
 
         return redirect('/')
