bedea90906e1273f35b6180eb2d3174ea8205b31
Kamini Parmar <kaminiparmar31489@gmail.com>
2026-05-30 17:16:43 +0530

    smtp setup

--- Base/views.py
+++ Base/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render, redirect
+from django.core.mail import send_mail
+from django.conf import settings
 from .models import Contact
 
+
 def home(request):
+
     if request.method == "POST":
+
         name = request.POST.get('name')
         email = request.POST.get('email')
         number = request.POST.get('number')
@@ -13,6 +18,27 @@ def home(request):
             email=email,
             number=number,
             content=content
+        )
+
+        subject = f"New Portfolio Contact - {name}"
+
+        message = f"""
+Name: {name}
+
+Email: {email}
+
+Phone: {number}
+
+Message:
+{content}
+"""
+
+        send_mail(
+            subject,
+            message,
+            settings.EMAIL_HOST_USER,
+            ["kaminiparmar31489@gmail.com"],
+            fail_silently=False,
         )
 
         return redirect('/')
